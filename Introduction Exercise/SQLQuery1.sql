4e6e196efa59cea28e2165aa0bb8794809e7e2dc
ZhechoZhechev <mishomishev024@gmail.com>
2023-01-14 19:16:11 +0200

    Introduction Exercise

--- Introduction Exercise/SQLQuery1.sql
+++ Introduction Exercise/SQLQuery1.sql
@@ -494,4 +494,81 @@ DROP DATABASE [SoftUni]
 GO
 
 RESTORE DATABASE [SoftUni] 
-	FROM DISK = N'C:\Program Files\Microsoft SQL Server\MSSQL16.MSSQLSERVER\MSSQL\Backup\SoftUniDB.bak'
+	FROM DISK = N'C:\Program Files\Microsoft SQL Server\Backup\SoftUniDB.bak'
+
+--18)Basic Insert
+
+USE [SoftUni]
+
+INSERT INTO [Towns] VALUES
+	('Sofia'),
+	('Plovdiv'),
+	('Varna'),
+	('Burgas')
+
+INSERT INTO [Departments] VALUES
+	('Engineering'),
+	('Sales'),
+	('Marketing'),
+	('Software Development'),
+	('Quality Assurance')
+
+INSERT INTO [Addresses] VALUES
+	('Default entry', 1)
+
+INSERT INTO [Employees] VALUES
+	('Ivan', 'Ivanov', 'Ivanov', '.NET Developer',4,'2013-02-01',3500.00,1),
+	('Petar','Petrov','Petrov','Senior Engineer',1,'2004-03-02',4000.00,1),
+	('Maria', 'Petrova', 'Ivanova', 'Intern',5, '2016-08-28', 525.25,1),
+	('Georgi','Teziev','Ivanov','CEO',2,'2007-12-09',3000.00,1),
+	('Peter','Pan','Pan','Intern',3,'2016-08-28',599.88,1)
+
+--19)Basic Select All Fields
+
+SELECT * FROM [Towns]
+
+SELECT * FROM [Departments]
+
+SELECT * FROM [Employees]
+
+--20)Basic Select All Fields and Order Them
+
+SELECT * FROM [Towns]	
+	ORDER BY [Name]
+
+SELECT * FROM [Departments]	
+	ORDER BY [Name]
+
+SELECT * FROM [Employees]
+	ORDER BY [Salary] DESC
+
+--21)Basic Select Some Fields
+
+SELECT [Name] FROM [Towns]
+	ORDER BY [Name]
+
+SELECT [Name] FROM [Departments]	
+	ORDER BY [Name]
+
+SELECT [FirstName], [LastName], [JobTitle], [Salary] FROM [Employees]
+	ORDER BY [Salary] DESC
+
+--22)Increase Employees Salary
+
+UPDATE [Employees]
+	SET [Salary] *= 1.1
+
+SELECT [Salary] FROM [Employees]
+
+--23)Decrease Tax Rate
+
+USE [Hotel]
+
+UPDATE [Payments]
+	SET [TaxRate] -= 0.03
+
+SELECT [Taxrate] FROM [Payments]
+
+--24)Delete All Records
+
+DELETE [Occupancies]
