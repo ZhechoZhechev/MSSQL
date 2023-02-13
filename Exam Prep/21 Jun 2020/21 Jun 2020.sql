38c7ef39fc24ec4f4baffbae49549f383344f27e
ZhechoZhechev <mishomishev024@gmail.com>
2023-02-13 17:57:46 +0200

    Exam Prep

--- Exam Prep/21 Jun 2020/21 Jun 2020.sql
+++ Exam Prep/21 Jun 2020/21 Jun 2020.sql
@@ -8,14 +8,14 @@ GO
 CREATE TABLE [Cities]
 (
 	[Id] INT PRIMARY KEY IDENTITY,
-	[Name] VARCHAR(20) NOT NULL,
+	[Name] NVARCHAR(20) NOT NULL,
 	[CountryCode] CHAR(2) NOT NULL
 )
 
 CREATE TABLE [Hotels]
 (
 	[Id] INT PRIMARY KEY IDENTITY,
-	[Name] VARCHAR(30) NOT NULL,
+	[Name] NVARCHAR(30) NOT NULL,
 	[CityId] INT FOREIGN KEY REFERENCES [Cities](Id) NOT NULL,
 	[EmployeeCount] INT NOT NULL,
 	[BaseRate] DECIMAL(18, 2)
@@ -58,6 +58,7 @@ CREATE TABLE [AccountsTrips]
 	[AccountId] INT FOREIGN KEY REFERENCES [Accounts](Id) NOT NULL,
 	[TripId] INT FOREIGN KEY REFERENCES [Trips](Id) NOT NULL,
 	[Luggage] INT CHECK([Luggage] >= 0) NOT NULL
+	PRIMARY KEY ([AccountId], [TripId])
 )
 
 -- 02. Insert
@@ -171,3 +172,157 @@ GROUP BY a.[Id], a.[Email], c.[Name]
 
 -- 10. GDPR Violation
 
+SELECT
+	   t.[Id],
+CONCAT(a.[FirstName], ' ', a.[MiddleName], ' ', a.[LastName]) AS [Full Name],
+      ci.[Name] AS [From],
+       c.[Name] AS [To],
+CASE WHEN
+t.[CancelDate] IS NULL THEN CONCAT(DATEDIFF(DAY, t.[ArrivalDate], t.[ReturnDate]), ' ', 'days')
+     ELSE 'Canceled'
+  END AS [Duration]
+    FROM [Trips] AS t
+    JOIN [AccountsTrips] AS [at]
+      ON [at].[TripId] = t.[Id]
+    JOIN [Accounts] AS a 
+	  ON [at].[AccountId] = a.[Id]
+    JOIN [Rooms] AS r
+    ON t.[RoomId] = r.[Id]
+    JOIN [Hotels] AS h
+    ON r.[HotelId] = h.[Id]
+    JOIN [Cities] AS c
+    ON h.[CityId] = c.[Id]
+    JOIN [Cities] AS ci
+    ON a.[CityId] = ci.[Id]
+ORDER BY [Full Name],
+	   t.[Id]
+
+SELECT
+    t.Id,
+    CONCAT(a.FirstName + ' ', a.MiddleName + ' ', a.LastName) AS FullName,
+    c.Name AS [From],
+    ci.Name AS [To],
+    CASE
+        WHEN t.CancelDate IS NOT NULL THEN 'Canceled'
+        ELSE CAST(DATEDIFF(DAY, t.ArrivalDate, t.ReturnDate) AS VARCHAR) + ' days'
+    END AS Duration
+FROM Trips AS t
+JOIN AccountsTrips AS at ON t.Id = at.TripId
+JOIN Accounts AS a ON at.AccountId = a.Id
+JOIN Cities AS c ON a.CityId = c.Id
+JOIN Rooms AS r ON t.RoomId = r.Id
+JOIN Hotels AS h ON r.HotelId = h.Id
+JOIN Cities AS ci ON h.CityId = ci.Id
+ORDER BY FullName, t.Id
+
+-- 11. Available Room
+
+GO
+
+CREATE OR ALTER FUNCTION udf_GetAvailableRoom(@hotelId INT , @date DATE, @people INT)
+RETURNS NVARCHAR(200)
+BEGIN
+
+
+	 DECLARE @roomNum INT
+	 SET @roomNum =
+	 ( 
+	 		SELECT TOP(1) r.[Id]
+		    FROM [Trips] AS t
+		    JOIN [Rooms] AS r ON t.[RoomId] = r.[Id]
+		    JOIN [Hotels] AS h ON r.[HotelId] = h.[Id]
+		    WHERE t.[CancelDate] IS NULL AND
+		    @date NOT BETWEEN t.[ArrivalDate] AND t.[ReturnDate] AND
+			YEAR(@Date) = YEAR(t.ArrivalDate) AND
+		    h.[Id] = @hotelId AND
+		    r.[Beds] >= @people
+		    ORDER BY r.[Price] DESC
+
+	 )
+	 	 IF(@roomNum IS NULL)
+		RETURN 'No rooms available'
+
+	 DECLARE @roomType NVARCHAR(20) 
+	 SET @roomType = (SELECT [Type] FROM [Rooms] WHERE [Id] = @roomNum)
+
+	 DECLARE @bedsNum INT 
+	 SET @bedsNum = (SELECT [Beds] FROM [Rooms] WHERE [Id] = @roomNum)
+
+	 DECLARE @roomPrice DECIMAL(18,2)= (SELECT Price
+        FROM Rooms
+        WHERE Id=@roomNum)
+     DECLARE @hotelBaseRate DECIMAL(5,2)= (SELECT BaseRate
+        FROM Hotels
+        WHERE Id=@HotelId)
+
+    DECLARE @TOTALPRICE DECIMAL(18,2)= (@hotelBaseRate + @roomPrice) * @people
+
+     RETURN  'Room ' + CAST(@roomNum AS varchar) + ': ' + @roomType + ' (' + CAST(@bedsNum AS varchar) + ' beds) - $' + CAST(@TOTALPRICE AS varchar)
+END
+
+GO
+
+SELECT dbo.udf_GetAvailableRoom(112, '2011-12-17', 2)
+
+SELECT dbo.udf_GetAvailableRoom(94, '2015-07-26', 3) 
+
+-- 12. Switch Room
+GO
+
+CREATE PROC usp_SwitchRoom(@TripId INT, @TargetRoomId INT)
+AS
+BEGIN
+	DECLARE @tripHotelID INT
+	SET @tripHotelID = 
+	(
+		SELECT r.[HotelId] FROM [Trips] AS t
+		JOIN [Rooms] AS r
+		ON t.[RoomId] = r.[Id]
+		WHERE t.[Id] = @TripId
+	)
+
+	DECLARE @roomHotelId INT
+	SET @roomHotelId = 
+	(
+		SELECT [HotelId] FROM [Rooms]
+		WHERE [Id] = @TargetRoomId
+	)
+
+	IF(@tripHotelID <> @roomHotelId) 
+		THROW 50001, 'Target room is in another hotel!', 1
+
+	DECLARE @targetRoomBeds INT
+	SET @targetRoomBeds = 
+	(
+		SELECT [Beds] FROM [Rooms]
+		WHERE [Id] = @TargetRoomId
+	)
+
+	DECLARE @people INT
+	SET @people = 
+	(
+		SELECT COUNT(a.[Id]) FROM [Trips] AS t
+		JOIN [AccountsTrips] AS [at]
+		ON [at].[TripId] = t.[Id]
+		JOIN [Accounts] AS a
+		ON [at].[AccountId] = a.[Id]
+		WHERE t.[Id] = @TripId
+	)
+
+	IF(@targetRoomBeds < @people)
+		THROW 50002, 'Not enough beds in target room!', 1
+
+	UPDATE [Trips]
+	SET [RoomId] = @TargetRoomId
+	WHERE [Id] = @TripId
+END
+
+GO
+
+EXEC usp_SwitchRoom 10, 7 
+
+EXEC usp_SwitchRoom 10, 8 
+
+EXEC usp_SwitchRoom 10, 11
+
+SELECT RoomId FROM Trips WHERE Id = 10 
